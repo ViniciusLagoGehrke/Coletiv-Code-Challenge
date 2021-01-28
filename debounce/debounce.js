8cc090e8506f2c5a4dc6c075ca19764d0cb42766
Vinicius Lago Gehrke <vinictus@gmail.com>
2021-01-28 02:35:39 +0000

    architecture

---
+++ debounce/debounce.js
@@ -0,0 +1,5 @@
+let a = () => console.log('foo')
+let b = debounce(a, 100)
+b()
+b()
+b() // only this call should invoke a()
---
+++ palindrome/palindrome.js
@@ -0,0 +1,6 @@
+/*write a function that given a string, return true if the string is a
+palindrome and false if it isn’t. Include spaces and punctuation in deciding if
+the string is a palindrome.*/
+
+palindrome('racecar') // true
+palindrome('table') // false
